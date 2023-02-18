2b052bf9e44e53ff2fa26beaf1410c0669652169
mustafasahin0 <sahinmustafaenes4@gmail.com>
2023-02-18 14:01:00 -0700

    added working with multiple elements

--- tests/home.test.ts
+++ tests/home.test.ts
@@ -5,6 +5,42 @@ test("Open home page and verify tite", async ({page}) => {
     await page.goto("https://www.fifa.com/fifaplus/en/home")
 
     await expect(page).toHaveTitle('FIFA+');  
+})
 
+test("Verifly logo is visible and has the alt title", async ({page}) => {
+    await page.goto("https://www.fifa.com/fifaplus/en/home");
+
+    const logo = page.locator('[href="/fifaplus/en/home"] img');
     
+    await expect(logo).toBeVisible();
+    await expect(logo).toHaveAttribute('title', 'Qatar 202');
+
+})
+
+test("Verify Navigation Links", async ({page}) => {
+    const expectedLinks = [
+      "FIFA WOMEN'S WORLD CUP 2023™",
+       "THE BEST",
+       "FIFA WORLD CUP 2022™",
+       "TEAMS",
+       "HIGHLIGHTS",
+       "SCORES & FIXTURES",
+       "KNOCKOUT & GROUPS",
+       "NEWS",
+       "ORIGINALS",
+       "ARCHIVE",
+       "PLAY ZONE",
+       "WHO AM I?",
+       "HEAD-TO-HEAD",
+       "TRIVIA",
+       "MATCH CENTRE",
+       "HIGHLIGHTS",
+       "LIVE STREAMING",];
+    
+    await page.goto("https://www.fifa.com/fifaplus/en/home");
+
+    const navLinks = page.locator('#mainLinksID a');
+    
+    await expect(navLinks.first()).toBeVisible();
+    expect(await navLinks.allInnerTexts()).toEqual(expectedLinks);
 })
